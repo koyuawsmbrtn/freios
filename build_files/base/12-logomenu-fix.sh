cbe7041608ee0dc592c7180397dd5aea6acd6cec
koyu <me@koyu.space>
2025-03-04 22:00:48 +0100

    fix: update logomenu script to compile schemas after installation

--- build_files/base/12-logomenu-fix.sh
+++ build_files/base/12-logomenu-fix.sh
@@ -5,3 +5,4 @@ rm -f /usr/share/gnome-shell/extensions/logomenu@aryan_k
 mkdir -p /usr/share/gnome-shell/extensions/logomenu@aryan_k
 unzip /usr/share/ublue-os/logomenu.zip -d /usr/share/gnome-shell/extensions/logomenu@aryan_k
 rm -f /usr/share/ublue-os/logomenu.zip
+glib-compile-schemas /usr/share/gnome-shell/extensions/logomenu@aryan_k/schemas/
