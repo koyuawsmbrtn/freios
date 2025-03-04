760c95ab58d898e7c5e6e709a86a26b42499d243
koyu <me@koyu.space>
2025-03-05 00:12:09 +0100

    fix: enhance installation scripts with grouping and error handling

--- build_files/base/12-logomenu-fix.sh
+++ build_files/base/12-logomenu-fix.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/bash
 
+echo "::group:: ===$(basename "$0")==="
+
+set -xeou pipefail
+
 wget "https://extensions.gnome.org/extension-data/logomenuaryan_k.v36.shell-extension.zip" -O /usr/share/ublue-os/logomenu.zip
 rm -f /usr/share/gnome-shell/extensions/logomenu@aryan_k
 mkdir -p /usr/share/gnome-shell/extensions/logomenu@aryan_k
 unzip /usr/share/ublue-os/logomenu.zip -d /usr/share/gnome-shell/extensions/logomenu@aryan_k
 rm -f /usr/share/ublue-os/logomenu.zip
 glib-compile-schemas /usr/share/gnome-shell/extensions/logomenu@aryan_k/schemas/
+sudo chmod -R 755 /usr/share/gnome-shell/extensions/logomenu@aryan_k
+sudo chown -R root:root /usr/share/gnome-shell/extensions/logomenu@aryan_k
+
+echo "::endgroup::"
