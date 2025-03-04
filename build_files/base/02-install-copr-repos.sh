d5a547831c995b91e2c21435795abfd992c85cf9
koyu <me@koyu.space>
2025-03-04 23:49:23 +0100

    fix: remove staging repository from installation script

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -4,9 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Add Staging repo
-dnf5 -y copr enable ublue-os/staging
-
 # Add Packages repo
 dnf5 -y copr enable ublue-os/packages
 
