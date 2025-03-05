ffda38f9daea372eea1466ef9da89eaa3292c845
koyu <me@koyu.space>
2025-03-05 03:05:17 +0100

    feat: add smile extension and update user setup for emoji picker shortcut

---
+++ build_files/base/14-emoji-picker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "::group:: ===$(basename "$0")==="
+
+set -xeou pipefail
+
+wget https://extensions.gnome.org/extension-data/smile-extensionmijorus.it.v10.shell-extension.zip -O /usr/share/ublue-os/smile-extension.zip
+mkdir -p /usr/share/gnome-shell/extensions/smile-extension@mijorus.it
+unzip /usr/share/ublue-os/smile-extension.zip -d /usr/share/gnome-shell/extensions/smile-extension@mijorus.it
+rm -f /usr/share/ublue-os/smile-extension.zip
+chmown -R root:root /usr/share/gnome-shell/extensions/smile-extension@mijorus.it
+chmod -R 755 /usr/share/gnome-shell/extensions/smile-extension@mijorus.it
+
+echo "::endgroup::"
