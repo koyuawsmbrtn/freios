db01e5363bbf42543dd1e51305ac67028364cf0a
koyu <me@koyu.space>
2025-03-07 00:48:45 +0100

    feat: add Just Perfection extension setup to user setup script

--- build_files/base/13-shell-fixes.sh
+++ build_files/base/13-shell-fixes.sh
@@ -18,4 +18,12 @@ rm -f /usr/share/ublue-os/windowIsReady_Remover.zip
 chown -R root:root /usr/share/gnome-shell/extensions/windowIsReady_Remover@nunofarruca@gmail.com
 chmod -R 755 /usr/share/gnome-shell/extensions/windowIsReady_Remover@nunofarruca@gmail.com
 
+wget https://extensions.gnome.org/extension-data/just-perfection-desktopjust-perfection.v34.shell-extension.zip -O /usr/share/ublue-os/just-perfection.zip
+mkdir -p /usr/share/gnome-shell/extensions/just-perfection-desktop@just-perfection
+unzip /usr/share/ublue-os/just-perfection.zip -d /usr/share/gnome-shell/extensions/just-perfection-desktop@just-perfection
+rm -f /usr/share/ublue-os/just-perfection.zip
+glib-compile-schemas /usr/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/
+chown -R root:root /usr/share/gnome-shell/extensions/just-perfection-desktop@just-perfection
+chmod -R 755 /usr/share/gnome-shell/extensions/just-perfection-desktop@just-perfection
+
 echo "::endgroup::"
