38dba44040b4784df5f971417faf7887d04c72df
koyu <me@koyu.space>
2025-04-19 06:41:10 +0200

    refactor: remove old issue templates and update references from bluefin to freios

--- iso_files/configure_iso.sh
+++ iso_files/configure_iso.sh
@@ -8,7 +8,7 @@ IMAGE_INFO="$(cat /usr/share/ublue-os/image-info.json)"
 IMAGE_TAG="$(jq -c -r '."image-tag"' <<< $IMAGE_INFO)"
 IMAGE_FLAVOR="$(jq -c -r '."image-flavor"' <<< $IMAGE_INFO)"
 
-OUTPUT_NAME="ghcr.io/ublue-os/bluefin"
+OUTPUT_NAME="ghcr.io/koyuawsmbrtn/freios"
 if [ "$IMAGE_FLAVOR" != "main" ] ; then
   OUTPUT_NAME="${OUTPUT_NAME}-${IMAGE_FLAVOR}"
 fi
@@ -20,7 +20,7 @@ copy_mode = "bootc"
 bootc_imgref = "containers-storage:$OUTPUT_NAME:$IMAGE_TAG"
 
 [distro]
-name = "Bluefin"
+name = "freiOS"
 
 [[postinstall]]
 module = "Script"
